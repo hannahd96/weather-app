7d94053d634ac825ef1a50c98d2b0bf756e80e42
hannahd96 <34348535+hannahd96@users.noreply.github.com>
2019-10-18 09:57:48 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,7 @@
 <template>
+  <!-- app is our main component -->
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   <!-- load all of the routes into the main area of the app -->
+   <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
